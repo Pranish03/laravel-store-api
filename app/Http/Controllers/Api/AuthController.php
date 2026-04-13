f27412a912a81d296e72823aa56cfa6ec9075c9a
Pranish Chaulagain <cpranishabc163@gmail.com>
2026-04-13 20:41:00 +0545

    Create user resource

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,8 +4,8 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\UserResource;
 use App\Models\User;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
@@ -23,40 +23,7 @@ class AuthController extends Controller
         return response()->json([
             'success' => true,
             'message' => 'Registered successfully',
-            'user' => $user->only(['id', 'name', 'email'])
-        ]);
-    }
-
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+            'user' => new UserResource($user)
+        ], 201);
     }
 }
